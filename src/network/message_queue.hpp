e58b62b853fbb71a8a545c2f9e20609fced0cf0e
Maxim Khailo <mempko@gmail.com>
2012-08-17 22:43:40 -0500

    added preliminary network library
    Ignore-this: e231ac040b179891e729d43e02d5a3bd
    
    added network library and made sure it compiles and links with firestr
    added basic message queue address parsing so that the create_message_queue factory
    function can decide with type of network queue is used. initial support will be
    for zeromq
    
    darcs-hash:20120818034340-c21fe-1c6a0c8f9d7f06d65cf7d895515eacaba157f7fc

---
+++ src/network/message_queue.hpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2012  Maxim Noah Khailo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef FIRESTR_NETWORK_MESSAGE_QUEUE_H
+#define FIRESTR_NETWORK_MESSAGE_QUEUE_H
+
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <string>
+#include <map>
+
+#include "util/bytes.hpp"
+#include "util/dbc.hpp"
+
+namespace fire 
+{
+    namespace network 
+    {
+        class message_queue
+        {
+            public:
+                virtual bool send(const util::bytes&) = 0;
+                virtual bool receive(util::bytes&) = 0;
+        };
+
+        typedef std::shared_ptr<message_queue> message_queue_ptr;
+        typedef std::map<std::string, std::string> queue_options;
+
+        message_queue_ptr create_message_queue(
+                const std::string& queue_address, 
+                const queue_options& default_options = queue_options());
+    }
+}
+
+#endif
