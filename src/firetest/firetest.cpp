126f5903948a3a78f45147455fc9bf57d2ab1282
Maxim Khailo <mempko@gmail.com>
2012-08-25 23:35:22 -0500

    added new firetest app
    Ignore-this: aa212773b339463d87bb3c3ae70ace4
    
    added new firetest app which will be used to test sending messages to firestr
    
    darcs-hash:20120826043522-c21fe-b969e31056afc136c32a8f6022f8dcae204796b8

---
+++ src/firetest/firetest.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2012  Maxim Noah Khailo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QApplication>
+
+#include "gui/testwin.hpp"
+
+#include <string>
+
+#include <boost/asio/ip/host_name.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+namespace ip = boost::asio::ip;
+namespace fg = fire::gui;
+
+po::options_description create_descriptions()
+{
+    po::options_description d{"Options"};
+
+    const std::string host = ip::host_name();
+    const std::string port = "6061";
+    const std::string fire_port = "6060";
+
+    d.add_options()
+        ("help", "prints help")
+        ("host", po::value<std::string>()->default_value(host), "host/ip of this machine") 
+        ("port", po::value<std::string>()->default_value(port), "port this machine will recieve messages on")
+        ("fire_port", po::value<std::string>()->default_value(fire_port), "port firestr is on");
+
+    return d;
+}
+
+po::variables_map parse_options(int argc, char* argv[], po::options_description& desc)
+{
+    po::variables_map v;
+    po::store(po::parse_command_line(argc, argv, desc), v);
+    po::notify(v);
+
+    return v;
+}
+
+int main(int argc, char *argv[])
+{
+    po::options_description desc = create_descriptions();
+    po::variables_map vm = parse_options(argc, argv, desc);
+    if(vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    QApplication a{argc, argv};
+
+    const std::string host = vm["host"].as<std::string>();
+    const std::string port = vm["port"].as<std::string>();
+    const std::string fire_port = vm["fire_port"].as<std::string>();
+
+    fg::test_window w(host, port, fire_port);
+    w.show();
+
+    return a.exec();
+}
