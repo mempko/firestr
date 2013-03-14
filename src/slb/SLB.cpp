fe4cc94b07c964e1b2daf1c7291cf39d538c71b5
Maxim Khailo <mempko@gmail.com>
2013-03-13 22:59:16 -0500

    fixed SLB compilation
    Ignore-this: e1134902e08ca8ae615f6120e68465c5
    
    darcs-hash:20130314035916-c21fe-cdadd5756993a57a13dea0199f1521aed8bbbfbb

--- src/slb/SLB.cpp
+++ src/slb/SLB.cpp
@@ -1733,12 +1733,6 @@ namespace SLB {
 
   bool Script::safeDoString(const char *o_code, const char *hint)
   {
-      std::string out;
-      return safeDoString2(o_code, out, hint);
-  }
-
-  bool Script::safeDoString2(const char *o_code, std::string& out, const char *hint)
-  {
     SLB_DEBUG_CALL;
     lua_State *L = getState();
     int top = lua_gettop(L);
