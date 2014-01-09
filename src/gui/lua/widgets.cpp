f863ccf7accc875a1b81c4927958aaaaa483fb6a
Maxim Khailo <mempko@gmail.com>
2014-01-09 08:46:27 -0600

    changed all copyright notices to 2014
    Ignore-this: 8746252326facf88c1ce40f25d0abbc6
    
    darcs-hash:20140109144627-c21fe-f45f7a42ddd77aa65794013dda361577830b828c

--- src/gui/lua/widgets.cpp
+++ src/gui/lua/widgets.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013  Maxim Noah Khailo
+ * Copyright (C) 2014  Maxim Noah Khailo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
