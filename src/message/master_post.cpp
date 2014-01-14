599b0f64159beded62d69b1ddcd2d1725424b2f8
Maxim Khailo <mempko@gmail.com>
2014-01-13 22:14:21 -0600

    increased some sleep times
    Ignore-this: 9bbd47efc0efa2f9539056d35eaa5c42
    
    darcs-hash:20140114041421-c21fe-6b890e556efa353bf765310c24f48c4ec708811b

--- src/message/master_post.cpp
+++ src/message/master_post.cpp
@@ -32,8 +32,8 @@ namespace fire
 
         namespace
         {
-            const double THREAD_SLEEP = 10; //in milliseconds 
-            const double QUIT_SLEEP = 50; //in milliseconds 
+            const double THREAD_SLEEP = 20; //in milliseconds 
+            const double QUIT_SLEEP = 500; //in milliseconds 
             const size_t POOL_SIZE = 30; //small pool size for now
             const size_t NONCE_SIZE = 16; //size of nonce added to every message.
         }
