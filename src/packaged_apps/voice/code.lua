5fbc8b0b0b8954c284692053c5f5e9bf01db9fc6
Maxim Khailo <mempko@gmail.com>
2014-07-02 09:22:36 -0500

    added voice chat example
    Ignore-this: 391ce7a8f2fed638c2bd8e46374b5720
    
    darcs-hash:20140702142236-c21fe-f4f08851a199ba93eebde51e61dd52d17df50624

---
+++ src/packaged_apps/voice/code.lua
@@ -0,0 +1,81 @@
+m = app:mic("got_sound", "mp3")
+s = app:speaker("mp3")
+m:start()
+
+muteb = app:button("mute mic")
+app:place(muteb, 0, 0)
+muteb:when_clicked("mute()")
+
+smuteb = app:button("mute speaker")
+app:place(smuteb, 0,1)
+smuteb:when_clicked("smute()")
+
+talkers = {}
+row = 2
+update_timer = app:timer(1000, "update()")
+
+function init_talker(id, name)
+	if talkers[id] == nil then  
+		local l = app:label(name.." talking")
+		app:place(l, row, 0)
+		row = row + 1
+		talkers[id] = {n=name, ticks = 0, label=l}
+	end
+end
+
+
+function got_sound(d)
+	local m = app:message()
+	m:set_type("s")
+	m:set_bin("d", d)
+	app:send(m)
+end
+
+app:when_message("s", "play_sound")
+function play_sound(m)
+	s:play(m:get_bin("d"))
+	talked(m:from():id(), m:from():name())
+end
+
+function mute()
+	m:stop()
+	muteb:when_clicked("unmute()")
+	muteb:set_text("unmute mic")
+end
+
+function unmute()
+	m:start()
+	muteb:when_clicked("mute()")
+	muteb:set_text("mute mic")
+end
+
+function smute()
+	s:mute()
+	smuteb:when_clicked("sunmute()")
+	smuteb:set_text("unmute speaker")
+end
+
+function sunmute()
+	s:unmute()
+	smuteb:when_clicked("smute()")
+	smuteb:set_text("mute speaker")
+end
+
+function talked(id, name)
+	if #id == 0 then return end
+	init_talker(id, name)
+	local u = talkers[id]
+	if u.ticks > 1 then
+		u.label:set_text(u.n.." talking")
+	end
+	u.ticks = 0
+end
+
+function update()
+	for id,u in pairs(talkers) do
+		u.ticks = u.ticks + 1
+		if u.ticks > 1 then
+			u.label:set_text(u.n.." not talking")
+		end
+	end
+end
