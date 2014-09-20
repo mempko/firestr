70bdf800882c8e6bf3767efdc0f440ad52f2cc40
mempko <mempko@gmail.com>
2014-09-20 00:21:29 -0500

    added basic vector clock utility.
    Ignore-this: 9ec48290cd48431b5dd789a04844af5d
    
    The idea is to expose vector clocks to apps so that they can easy implement
    eventual consistency and be able to partially order events.
    
    darcs-hash:20140920052129-c21fe-16db8db99a4aa9c4c50f275057d57df91a681ed9

---
+++ src/util/vclock.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2014  Maxim Noah Khailo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give 
+ * permission to link the code of portions of this program with the 
+ * Botan library under certain conditions as described in each 
+ * individual source file, and distribute linked combinations 
+ * including the two.
+ *
+ * You must obey the GNU General Public License in all respects for 
+ * all of the code used other than Botan. If you modify file(s) with 
+ * this exception, you may extend this exception to your version of the 
+ * file(s), but you are not obligated to do so. If you do not wish to do 
+ * so, delete this exception statement from your version. If you delete 
+ * this exception statement from all source files in the program, then 
+ * also delete it here.
+ */
+
+#include "util/vclock.hpp"
+
+namespace fire 
+{
+    namespace util 
+    {
+        dict to_dict(const sclock& cs)
+        {
+            dict d;
+            for(const auto& c : cs)
+                d[c.first] = c.second;
+            return d;
+        }
+
+        sclock to_sclock(const dict& d)
+        {
+            sclock c;
+            for(const auto& v : d)
+                c[v.first] = v.second.as_size();
+            return c;
+        }
+
+        dict to_dict(const tracked_sclock& c)
+        {
+            dict d{
+                {"i", c.id()}, 
+                {"v", to_dict(c.clock())}
+            };
+            return d;
+        }
+
+        tracked_sclock to_tracked_sclock(const dict& d)
+        {
+            auto id = d["i"].as_string();
+            tracked_sclock c{
+                d["i"].as_string(), 
+                to_sclock(d["v"].as_dict())
+            };
+            return c;
+        }
+    }
+}
