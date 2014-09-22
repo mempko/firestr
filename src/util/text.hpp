4bdcdd9d003b5da00995a262f09b50e7ef480e67
mempko <mempko@gmail.com>
2014-09-22 18:01:52 -0500

    added text utility for doing 3 way merge
    Ignore-this: 708d33daed01c84636e2acf78ff77638
    
    darcs-hash:20140922230152-c21fe-2460d2eae371477a554f03d38ca1bf4dbd3228e1

---
+++ src/util/text.hpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2014  Maxim Noah Khailo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either vedit_refsion 3 of the License, or
+ * (at your option) any later vedit_refsion.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give 
+ * permission to link the code of portions of this program with the 
+ * Botan library under certain conditions as described in each 
+ * individual source file, and distribute linked combinations 
+ * including the two.
+ *
+ * You must obey the GNU General Public License in all respects for 
+ * all of the code used other than Botan. If you modify file(s) with 
+ * this exception, you may extend this exception to your version of the 
+ * file(s), but you are not obligated to do so. If you do not wish to do 
+ * so, delete this exception statement from your version. If you delete 
+ * this exception statement from all source files in the program, then 
+ * also delete it here.
+ */
+
+#ifndef FIRESTR_UTIL_TEXT_H
+#define FIRESTR_UTIL_TEXT_H
+
+#include <string>
+
+namespace fire 
+{
+    namespace util 
+    {
+        /**
+         * Merges three strings into one.
+         * @return false if there is a conflict
+         */
+        bool merge(
+                const std::string& a, 
+                const std::string& b, 
+                const std::string& c,
+                std::string& out); 
+    }
+}
+#endif
