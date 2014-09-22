bbd189dca586216c1eaf04710660ca094c12910b
Maxim Khailo <mempko@gmail.com>
2014-09-22 18:02:17 -0500

    added new concurrent string implementation
    Ignore-this: c52973eb14c77a20e32484e8e1f533b5
    
    Concurrent string combines a string data type, vector clock, and merge function to
    make maintaining replicated string data easier.
    
    It is able to determine when two strings were changed concurrent and perform  merge
    
    darcs-hash:20140922230217-c21fe-fcddbc9797c1bb29b0f286c416a7d357d00943e9

---
+++ src/util/crstring.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2014  Maxim Noah Khailo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either vedit_refsion 3 of the License, or
+ * (at your option) any later vedit_refsion.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give 
+ * permission to link the code of portions of this program with the 
+ * Botan library under certain conditions as described in each 
+ * individual source file, and distribute linked combinations 
+ * including the two.
+ *
+ * You must obey the GNU General Public License in all respects for 
+ * all of the code used other than Botan. If you modify file(s) with 
+ * this exception, you may extend this exception to your version of the 
+ * file(s), but you are not obligated to do so. If you do not wish to do 
+ * so, delete this exception statement from your version. If you delete 
+ * this exception statement from all source files in the program, then 
+ * also delete it here.
+ */
+
+#include "util/crstring.hpp"
+#include "util/text.hpp"
+#include "util/dbc.hpp"
+#include "util/log.hpp"
+
+namespace fire 
+{
+    namespace util 
+    {
+        cr_string::cr_string(const std::string& id) : _c{id} {}
+        cr_string::cr_string(const tracked_sclock& c, const std::string& s) : _c{c}, _s{s} {}
+
+        const std::string& cr_string::str() const { return _s; }
+        const tracked_sclock& cr_string::clock() const { return _c; }
+
+        void cr_string::init_set(const std::string& s) { _s = s;}
+        void cr_string::set(const std::string& s) { _s = s; _c++;}
+
+        bool cr_string::merge(const cr_string& o)
+        {
+            auto cmp = o._c.compare(_c);
+            bool merged = false;
+
+            //merge clocks
+            _c += o._c;
+
+            //merge strings
+            switch(cmp)
+            {
+                case -1: /*do nothing */ break;
+
+                //string is newer then set current to new
+                case 1: _s = o._s; break;
+
+                //string is concurrent, do 3 way merge
+                case 0: 
+                    {
+                        //check to see if we have last seen string from other node
+                        //and use it as base string, otherwise use other nodes string
+                        auto a = o._s;
+                        auto b = o._s;
+                        auto c = _s;
+
+                        bool merged = util::merge(a, b, c, _s);
+
+                        //if we can't merge then pick other string
+                        //TODO: allow user defined strategies
+                        if(!merged) _s = o._s;
+
+                        merged = true;
+                    }
+                    break;
+                default:
+                    CHECK(false && "missed case");
+            }
+            return merged;
+        }
+
+    }
+}
