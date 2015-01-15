ea8708d2ca49c94e8f22d5821f0299d639032618
mempko <mempko@gmail.com>
2015-01-14 20:22:33 -0600

    updated client version to 5
    Ignore-this: 807db6ff2865d7249a07f11f8a832fd
    
    The version is now 0.5. Bumped it because lua 5.3 support makes some apps incompatible
    with version 0.4
    
    darcs-hash:20150115022233-c21fe-3d87c6bfc9e75f032c88ccb8c266d5cc28a5ce92

--- src/util/version.cpp
+++ src/util/version.cpp
@@ -38,7 +38,7 @@ namespace fire
     namespace util
     {
         const int PROTOCOL_VERSION = 0;
-        const int CLIENT_VERSION = 4;
+        const int CLIENT_VERSION = 5;
 
         std::string version_string()
         {
