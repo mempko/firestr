1c885aaa84329de71b03fbd4875cbf3b2e4fbb12
Maxim Khailo <mempko@gmail.com>
2015-04-26 00:30:30 -0500

    added new random number util
    Ignore-this: 5f0bee156e90c3b1d30dc6fb0c38fc44
    
    darcs-hash:20150426053030-c21fe-5d85d8eff7fe5389901e1f7c6e7efe3c7b567646

---
+++ src/util/rand.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2015  Maxim Noah Khailo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give 
+ * permission to link the code of portions of this program with the 
+ * Botan library under certain conditions as described in each 
+ * individual source file, and distribute linked combinations 
+ * including the two.
+ *
+ * You must obey the GNU General Public License in all respects for 
+ * all of the code used other than Botan. If you modify file(s) with 
+ * this exception, you may extend this exception to your version of the 
+ * file(s), but you are not obligated to do so. If you do not wish to do 
+ * so, delete this exception statement from your version. If you delete 
+ * this exception statement from all source files in the program, then 
+ * also delete it here.
+ */
+
+#include "util/rand.hpp"
+
+#include "util/dbc.hpp"
+
+#include <ctime>
+#include <random>
+#include <mutex>
+
+namespace fire
+{
+    namespace util
+    {
+        namespace
+        {
+            std::mutex RAND_MUTEX;
+            std::mt19937 RNG;
+            bool SEEDED = false;
+        }
+
+        void init_rand()
+        {
+            REQUIRE_FALSE(SEEDED);
+            RNG.seed(std::time(0));
+            SEEDED = true;
+
+            ENSURE(SEEDED);
+        }
+
+        uint32_t rand(uint32_t min, uint32_t max)
+        {
+            REQUIRE_LESS(min, max);
+            REQUIRE(SEEDED);
+
+            std::lock_guard<std::mutex> lock(RAND_MUTEX);
+            std::uniform_int_distribution<uint32_t> dist(min,max); 
+
+            auto v = dist(RNG);
+            ENSURE_RANGE(v, min, max);
+            return v;
+        }
+    }
+}
