c716d0c0c6c1532f730c6394b7ac874fc20aa969
mempko <mempko@gmail.com>
2015-04-27 22:28:55 -0500

    version update to 0.9
    Ignore-this: 9bb52de83da3051e3bc618d9e097f927
    
    darcs-hash:20150428032855-c21fe-0042b8ad0db12f70980258e3c31d309dcb0943d4

--- src/util/version.cpp
+++ src/util/version.cpp
@@ -38,7 +38,7 @@ namespace fire
     namespace util
     {
         const int PROTOCOL_VERSION = 0;
-        const int CLIENT_VERSION = 8;
+        const int CLIENT_VERSION = 9;
 
         std::string version_string()
         {
