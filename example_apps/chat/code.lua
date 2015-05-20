31d0ca491a420315ff96fb127fec0a7378d2ef19
mempko <mempko@gmail.com>
2015-05-19 22:04:56 -0500

    chatty chat matches new tutorial
    Ignore-this: e60555115fb8826aadc253a892c71826
    
    darcs-hash:20150520030456-c21fe-bbb5c799a6e94a85a72139d1ab3cf010af4c4a3c

--- example_apps/chat/code.lua
+++ example_apps/chat/code.lua
@@ -2,30 +2,35 @@
 app:height(200)
 app:width(300)
 
-m = app:list()
-app:place_across(m, 0, 0, 1, 2)
+msgs = app:list()
+app:place_across(msgs, 0, 0, 1, 2)
 
-e = app:edit("")
-app:place(e, 1, 0)
+ed = app:edit("")
+app:place(ed, 1, 0)
 
-s = app:button("send")
-app:place(s, 1, 1)
+snd = app:button("send")
+app:place(snd, 1, 1)
 
-s:when_clicked("send_message")
-e:when_finished("send_message")
+snd:when_clicked("clicked_send_message()")
+ed:when_finished("send_message")
 
 app:when_message_received("got")
+
+function clicked_send_message()
+    send_message(ed:text())
+end
 
 function send_message(text)
     if #text== 0 then
         return
     end
+
     msg = app:message()
     msg:set("text", text)
     app:send(msg)
     add_message("me", text)
 
-    e:set_text("")
+    ed:set_text("")
 end
 
 function got(msg)
@@ -37,6 +42,6 @@ end
 function add_message(name, text)
     local t = "<b>"..name..":</b> "..text
     local nl = app:label(t)
-    m:add(nl)
+    msgs:add(nl)
     app:alert()
 end
